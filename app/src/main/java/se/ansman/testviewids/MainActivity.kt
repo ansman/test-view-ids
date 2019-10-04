c48f53496ac42e7164c5f33a004fc043dc482ddb
Nicklas Ansman Giertz <nicklas@ansman.se>
2019-10-04 09:23:46 -0400

    Add the source

---
+++ app/src/main/java/se/ansman/testviewids/MainActivity.kt
@@ -0,0 +1,12 @@
+package se.ansman.testviewids
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
